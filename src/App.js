897e8cdbc87afaabe5b130df61ae19d2dfa2f810
Ankit Rawani <ankitgdes@gmail.com>
2022-05-29 15:18:29 +0530

    Add: API call function, CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
+import Searchbar from './components/Searchbar';
+import Results from './components/Results';
+import useDebounce from './hooks/useDebounce';
+
+function searchGIF(query, offset) {
+  return fetch(`http://api.giphy.com/v1/gifs/search?api_key=GlVGYHkr3WSBnllca54iNt0yFbjz7L65&q=${query}&limit=6&offset=${offset}`)
+    .then(res => res.json())
+    .catch(err => {
+      console.error(err);
+      return [];
+    });
+}
 
 function App() {
+  const [query, setquery] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [data, setdata] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [total, setTotal] = useState(0);
+  
+  const debouncedQuery = useDebounce(query, 1000);
+
+  useEffect(() => {
+    if(debouncedQuery) {
+      searchGIF(debouncedQuery, offset).then(res => {
+        if(res.meta.status == 200) {
+          setIsLoaded(true);
+          setdata(res.data);
+          setTotal(res.pagination.total_count);
+          console.log(res);
+        }
+        else {
+          setIsLoaded(false);
+          setdata([]);
+        }
+      });
+    }
+    else {
+      setdata([]);
+      setIsLoaded(true);
+    }
+
+  }, [debouncedQuery]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Searchbar query={query} setquery={setquery} />
+      <Results isLoaded={isLoaded} data={data} total={total} setOffset={setOffset} currentOffet={offset} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './fonts/apercu_bold_pro.otf';
+import './fonts/apercu_regular_pro.otf';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
